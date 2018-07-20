dafdcb3cdbf35b1f637629616653e0fba4913abe
ricardocarosa <rickcarosa@yahoo.com>
2018-07-20 16:01:48 -0400

    changed server file

---
+++ server/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express')
+    , bodyParser = require('body-parser')
+    , app = express()
+    , controller = require('./controller')
+    , cors = require('cors')
+    , session = require('express-session')
+    , passport = require('passport')
+    , Auth0Strategy = require('passport-auth0')
+    , massive = require('massive')
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const {
+    SERVER_PORT
+} = process.env
+
+app.listen(SERVER_PORT, () => console.log(`Fast and Furious`))
