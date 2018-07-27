e23a84651f99623253fae311d0e777716577b255
Cody Tomesch <tomeschcody@gmail.com>
2018-07-27 14:52:23 -0600

    video is in position!

--- src/components/baseSpecs/BaseSpecs.js
+++ src/components/baseSpecs/BaseSpecs.js
@@ -4,10 +4,18 @@ import glamorous from 'glamorous'
 const Styles = glamorous.div({
     position: 'relative',
     top: 'calc(100vh - 56px)',
-    height: '100vh',
-    backgroundColor: 'black',
+    height: '200vh',
+    width: '100vw'
+,    backgroundColor: 'black',
     color: 'white',
-    fontSize: '50px'
+    fontSize: '50px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+})
+
+let VideoDiv = glamorous.div({
+    padding: '100px',
 })
 
 class BaseSpecs extends Component {
@@ -15,9 +23,10 @@ class BaseSpecs extends Component {
         console.log('pizza')
         return(
             <Styles>
-                <div>
-                    <video src='https://www.tesla.com/ns_videos/roadster_videos/roadster-loop-imperial.mp4?20180329' autoplay="" loop="" muted="" playsinline="" ></video>
-                </div>
+                <VideoDiv>
+                    <video src='https://www.tesla.com/ns_videos/roadster_videos/roadster-loop-imperial.mp4?20180329' autoplay="" loop muted="" playsinline="" style={{height: '216px', width: 'auto'}}></video>
+                    
+                </VideoDiv>
             </Styles>
         ) 
     }
