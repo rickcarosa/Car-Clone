745a87c17cf135f112bbcdd03bc02368d54bb3bf
ricardocarosa <rickcarosa@yahoo.com>
2018-07-30 17:26:46 -0400

    speedometer done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {HashRouter, Switch, Route} from 'react-router-dom';
 import Home from './components/home/Home';
