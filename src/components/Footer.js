c6ee958e540844cf117d55905318b6fe96f40fb5
ricardocarosa <rickcarosa@yahoo.com>
2018-08-12 15:20:01 -0400

    footer start

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import glamorous from 'glamorous';
+
+const FooterDiv = glamorous.div({
+    
+})
+
+class Footer extends Component {
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <FooterDiv>
+
+            </FooterDiv>
+        )
+    }
+}
+
+export default Footer;
