39022486a208a553b0e819e8ee43bf73fc8dacc1
Cody Tomesch <tomeschcody@gmail.com>
2018-08-22 12:00:49 -0700

    removed the unnecessary

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -45,7 +45,6 @@ const ListContents = glamorous.li({
     ':nth-child(4)':{
         color: 'white',
         opacity: '0.7',
-        // fontFamily: 'Gotham-Medium'
     },
     ':hover':{
         color: 'white',
